8d9ea379e555e2b6992c42a45cac4f136fa14188
simalyos <sambyto@gmail.com>
2022-12-21 13:29:57 +0100

    new file:   migrations/Version20221221122830.php 	modified:   src/Entity/Ingredient.php 	modified:   templates/ingredient/index.html.twig

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -13,6 +13,12 @@ class Ingredient
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $quantity = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -25,6 +31,18 @@ class Ingredient
     public function setType(string $type): self
     {
         $this->type = $type;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?string
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(string $quantity): self
+    {
+        $this->quantity = $quantity;
 
         return $this;
     }
