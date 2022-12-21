39be2bf91efdc4d86e25bd5972c99c376bb7d6b4
simalyos <sambyto@gmail.com>
2022-12-21 17:25:27 +0100

    new file:   migrations/Version20221221143733.php 	new file:   migrations/Version20221221151312.php 	modified:   src/Entity/Recette.php 	modified:   src/Form/RecetteType.php 	modified:   templates/recette/_form.html.twig 	modified:   templates/recette/index.html.twig

--- src/Entity/Recette.php
+++ src/Entity/Recette.php
@@ -13,8 +13,28 @@ class Recette
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->getName();
     }
 }
--- src/Form/RecetteType.php
+++ src/Form/RecetteType.php
@@ -3,17 +3,61 @@
 namespace App\Form;
 
 use App\Entity\Recette;
+use App\Entity\Ingredient;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class RecetteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ;
+        ->add('name', TextType::class, [
+            'attr'=> [
+                'class' => 'form-control'
+            ], 
+            'label' => 'Nom',
+            'label_attr' => [
+                'class' => 'form-label'
+            ]
+        ])
+
+
+        ->add('ingredient', EntityType::class, [
+            'class'     => Ingredient::class,
+            'expanded'  => true,
+            'multiple'  => true,
+            // 'entry_type' => IngredientType::class, 
+            // 'entry_options' => ['label' => false],
+            'attr'=> [
+                'class' => 'form-control'
+            ], 
+            'label' => 'Ingrédients',
+            'label_attr' => [
+                'class' => 'form-label'
+            ],
+            'constraints' => [
+                new NotBlank()
+            ]
+        ])
+    
+
+        ->add('submit', SubmitType::class, [
+            'attr' => [
+                'class' => 'btn btn-success mt-4'
+            ], 
+            'label' => 'Ajouter une recette'
+        ])
+    ;
     }
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
@@ -22,3 +66,5 @@ class RecetteType extends AbstractType
         ]);
     }
 }
+
+
